fb1fafe3e6532dbd308a1f762e1304b2bba02ffc
munshpunsh <47504634+munshpunsh@users.noreply.github.com>
2019-08-15 23:30:28 +0400

    Add files via upload

---
+++ precache-manifest.45cfbcca27c9fc188b7843fca42e6ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c618e535a8ab8cdce7b8390b023ada2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa18d771452e25f6657d",
+    "url": "/static/css/main.7077645c.chunk.css"
+  },
+  {
+    "revision": "37cdf20488f883a498e3",
+    "url": "/static/js/2.c71989d9.chunk.js"
+  },
+  {
+    "revision": "fa18d771452e25f6657d",
+    "url": "/static/js/main.33ab1762.chunk.js"
+  },
+  {
+    "revision": "b3108d08d63648b83d99",
+    "url": "/static/js/runtime~main.4e21cc9b.js"
+  }
+]);
